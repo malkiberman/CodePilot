17a644b0721181a6fc96224f516fb2c7c0796746
malkiberman <559120b@gmail.com>
2025-03-13 12:26:30 +0200

    m

--- CodePilot-react/src/components/uploadFile.tsx
+++ CodePilot-react/src/components/uploadFile.tsx
@@ -5,8 +5,7 @@ import { uploadFile } from "../services/codeFileService";
 const UploadFile = () => {
   const [file, setFile] = useState<File | null>(null);
   const [message, setMessage] = useState("");
-  const [useremail, setEmail] = useState("");
-const [password, setPassword] = useState("");
+
   const handleFileUpload = async () => {
     if (!file) {
       setMessage("Please select a file first.");
@@ -26,8 +25,7 @@ const [password, setPassword] = useState("");
     <div>
       <h2>Upload Code File</h2>
       <input type="file" onChange={(e) => setFile(e.target.files?.[0] || null)} />
-      <input type="text" placeholder=" email" onChange={(e) => setEmail(e.target.value)} />
-      <input type="text" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+      
       <button onClick={handleFileUpload}>Upload</button>
       {message && <p>{message}</p>}
     </div>
