9baa8217da7e08b62870633e4720cce6089fdcd8
malkiberman <559120b@gmail.com>
2025-03-25 21:12:12 +0200

    oved bsd lo render lo izuv

--- CodePilot-react/src/config.ts
+++ CodePilot-react/src/config.ts
@@ -1,3 +1,5 @@
-const API_BASE_URL = "https://localhost:7099/api"; // כאן שימי את הכתובת של ה-API שלך
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+//  ||"https://localhost:7099/api"
+ ; // כאן שימי את הכתובת של ה-API שלך
 
 export default API_BASE_URL;
