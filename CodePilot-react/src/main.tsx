6d5a6caa2ee1a76cc522db579de54c3dc593f61e
malkiberman <559120b@gmail.com>
2025-03-29 21:59:21 +0300

    iuyf

--- CodePilot-react/src/main.tsx
+++ CodePilot-react/src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL||import.meta.env.REACT_APP_API_BASE_URL);
-console.log(import.meta.env);
+console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
+console.log("API BASE URL:",import.meta.env.REACT_APP_API_BASE_URL);
+console.log(process.env.REACT_APP_API_BASE_URL);
 console.log(process.env);
 
 
